a8f91468dfb098b3a09b252c276fd62a06aed6b5
Jelle Vermandere <jelle.vermandere@kuleuven.be>
2022-12-16 15:06:18 +0100

    screen space layout fix

--- Assets/Scripts/0_GlobalScripts/ScreenSpaceTransform.cs
+++ Assets/Scripts/0_GlobalScripts/ScreenSpaceTransform.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ScreenSpaceTransform : MonoBehaviour
 {
     [SerializeField]
+    private Vector2 refResolution = new Vector2(1920, 1080);
+    [SerializeField]
     private Vector3 offset;
     [SerializeField]
     private bool startRight = false;
@@ -13,7 +15,12 @@ public class ScreenSpaceTransform : MonoBehaviour
 
     void SetPosition()
     {
-        Vector2 screenPos = new Vector2(startRight ? Camera.main.pixelWidth - offset.x : offset.x, startTop ? Camera.main.pixelHeight - offset.y : offset.y);
+        Camera cam = Camera.main;
+        Rect camSize = cam.pixelRect;
+
+        Vector3 scaledOffset = Vector2.Scale(offset, Vector2.Scale(new Vector2(camSize.width, camSize.height), new Vector2(1 / refResolution.x, 1 / refResolution.y)));
+        
+        Vector2 screenPos = new Vector2(startRight ? cam.pixelWidth - scaledOffset.x : scaledOffset.x, startTop ? cam.pixelHeight - scaledOffset.y : scaledOffset.y);
 
 
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, offset.z));
